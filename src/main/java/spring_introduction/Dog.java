980e57c425294935a0751c6f1951140d8f670494
Rusgar1 <grr7727@gmail.com>
2023-04-18 23:32:53 +0400

    fix: applicationContext2.xml file - add scopes beans

---
+++ src/main/java/spring_introduction/Dog.java
@@ -0,0 +1,19 @@
+package spring_introduction;
+
+public class Dog implements Pet{
+    private String name;
+    public Dog() {
+        System.out.println("Dog bean is created");
+    }
+    @Override
+    public void say() {
+        System.out.println("Bow-wow");
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
